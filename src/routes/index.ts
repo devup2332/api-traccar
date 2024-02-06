75c2de8fdf894a7b14bfd191452a193af4491a0f
Diego Rojas <devup2332@gmail.com>
2024-02-05 21:36:56 -0500

    master: Edited routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { GENERAR_PARADEROS_CONTROLLER } from "../controllers/generarParaderos.controller";
+import { GET_PARADEROS } from "../controllers/getParaderos";
 
 const router = Router();
 
 router.get("/generarParaderos", GENERAR_PARADEROS_CONTROLLER);
-router.get("/paraderos", GENERAR_PARADEROS_CONTROLLER);
+router.get("/paraderos", GET_PARADEROS);
 
 export default router;
