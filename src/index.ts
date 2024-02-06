73097d9663088433e75381ae383634a5618b8bb3
Diego Rojas <devup2332@gmail.com>
2024-02-05 21:26:58 -0500

    master: Adde cors lib

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { environments } from "./config/environment";
 import indexRouter from "./routes";
 import morgan from "morgan";
+import cors from "cors";
 
 const server = express();
 
+server.use(cors());
 server.use(morgan("dev"));
 server.use("/api", indexRouter);
 
