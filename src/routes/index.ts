187e3030c759b112158a37d135e3ccb5160dedef
Diego Rojas <devup2332@gmail.com>
2024-02-05 21:28:30 -0500

    d

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ import { GENERAR_PARADEROS_CONTROLLER } from "../controllers/generarParaderos.co
 const router = Router();
 
 router.get("/generarParaderos", GENERAR_PARADEROS_CONTROLLER);
+router.get("/paraderos", GENERAR_PARADEROS_CONTROLLER);
 
 export default router;
